c84ceb393118ffdc1bc579364520d75e44d795b1
Varghese Baby <Vbpoulose@gmail.com>
2026-02-13 12:48:47 -0500

    Go: variable

---
+++ variable/varFeb13.go
@@ -0,0 +1,16 @@
+package main
+import ("fmt")
+
+func main() {
+	var a, b= 86, "Varghese"
+	c , d:=25, "Baby"
+
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+	fmt.Println(d)
+}
+
+
+// Important Rule in Go
+/*variables count == values count*/
