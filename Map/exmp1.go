fec0a76d71d28b6b833e542b39a577b08b89287b
Varghese Baby <Vbpoulose@gmail.com>
2026-02-14 19:43:36 -0500

    Go: Parm&Argu_Map_Infer_UpdatedNotes

--- Map/exmp1.go
+++ Map/exmp1.go
@@ -14,6 +14,20 @@ func main() {
   fmt.Printf("b\t%v\n", b)
 }
 
+// A map is an unordered and changeable collection that does not allow duplicates.
+/*Each element in a map is a key:value pair.
+
+A map is an unordered and changeable collection that does not allow duplicates.
+
+The length of a map is the number of its elements. You can find it using the len() function.
+
+The default value of a map is nil.
+
+Maps hold references to an underlying hash table.
+
+Go has multiple ways for creating maps.*/
+
+
 /*Var a = with infer can used Insid & outside Function
 b := Only used inside Function 
 
